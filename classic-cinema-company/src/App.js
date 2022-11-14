601947128ca0b3903691b444da742030a5ef3e65
Michael Harrison <ghost1220@gmail.com>
2022-11-14 15:54:38 +0000

    Initial commit 3

--- classic-cinema-company/src/App.js
+++ classic-cinema-company/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
+import Home from './components/home';
+import Schedule from './components/schedule';
+import Signup from './components/signup';
 
 function App() {
   return (
     <div className="App">
         <h1>Classic Cinema Company</h1>
+        <Header />
+        <Home />
+        <Schedule />
+        <Signup />
+        <Footer />
     </div>
   );
 }

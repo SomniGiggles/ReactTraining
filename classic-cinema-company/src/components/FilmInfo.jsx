32254b518a06e489d156c138b9ac7f77c669069e
Michael Harrison <ghost1220@gmail.com>
2022-11-15 10:49:15 +0000

    Added pictures for films

--- classic-cinema-company/src/components/FilmInfo.jsx
+++ classic-cinema-company/src/components/FilmInfo.jsx
@@ -6,7 +6,7 @@ const FilmInfo = (props) => {
 
     return (
         <div>
-            <img alt="movie_poster"></img>
+            <img src={props.imageLink} alt={props.imageLink}></img>
             <h3>{props.name}</h3><h4>{props.ageCert}</h4>
             <div className="film-description">{props.description}</div>
             <div className="film-container">{allTimes}</div>

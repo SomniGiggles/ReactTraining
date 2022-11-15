25411f8603856462bd4674e59c7200d3c71e92f5
andrewp2-digital <Andrew.P2@digital.ncsc.gov.uk>
2022-11-15 12:06:42 +0000

    fix signup console error and label text

--- classic-cinema-company/src/components/signup.jsx
+++ classic-cinema-company/src/components/signup.jsx
@@ -6,17 +6,17 @@ const SignUp = () => {
     <h1>JOIN THE CLUB</h1>
     <div className='formDiv'>
         <form className='joinForm'>
-            <label for="title">Title: </label>
+            <label htmlFor="title">Title: </label>
             <input type='text' id='title' name='title' placeholder='Title'/> <br />
-            <label for="fname">First name: </label>
+            <label htmlFor="fname">First name: </label>
             <input type='text' id='fname' name='fname' placeholder='First Name' /> 
-            <label for="lname">Last name: </label>
+            <label htmlFor="lname">Last name: </label>
             <input type='text' id='lname' name='lname' placeholder='Last Name' /> <br />
-            <label for="email">Email: </label>
+            <label htmlFor="email">Email: </label>
             <input type='text' id='email' name='email' placeholder='name@example.com' /> <br />
-            <label for="phone">Phone: </label>
+            <label htmlFor="phone">Phone: </label>
             <input type='text' id='phone' name='phone' placeholder='012345678910' /> <br />
-            <label for="dob">Date of Birth: </label>
+            <label htmlFor="dob">Date of Birth: </label>
             <input type='text' id='dob' name='dob' placeholder='DD/MM/YYYY' /> <br />
             <button>
                 <NavLink to='/'>Submit</NavLink>

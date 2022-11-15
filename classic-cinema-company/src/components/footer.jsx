35ce1593c1b6d5ff00166bf42aec25203e446736
Michael Harrison <ghost1220@gmail.com>
2022-11-15 12:07:44 +0000

    Added images for footer

--- classic-cinema-company/src/components/footer.jsx
+++ classic-cinema-company/src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer-background">
             <div className="film-container">
-                <img alt="footer-logo"/>
+                <img src="/footer-logo.png" width="200px" alt="footer-logo"/>
                 <div className="film-description">
                     <h3>MORE INFO</h3>
                     <button>About us</button>
@@ -13,15 +13,15 @@ const Footer = () => {
                 </div>
                 <div className="film-description">
                     <h3>FIND US</h3>
-                    <img alt="map"/>
+                    <img src="/footer-map.png" width="100px" alt="map"/>
                     <p>Lorem ipsum dolar sit amet, consectetuer</p>
                 </div>
                 <div className="film-description">
                     <h3>FOLLOW US</h3>
-                    <img alt="twitter-icon"/>
-                    <img alt="instagram-icon"/>
-                    <img alt="facebook-icon"/>
-                    <img alt="youtube-icon"/>
+                    <img src="/twitter.png" alt="twitter-icon"/>
+                    <img src="/instagram.png" alt="instagram-icon"/>
+                    <img src="/facebook.png" alt="facebook-icon"/>
+                    <img src="/youtube.png" alt="youtube-icon"/>
                 </div>
             </div>
             <p>Copyright C The Classic Cinema Company Ltd 2020. All rights reserved.</p>

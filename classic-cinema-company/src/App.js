0567b856cefa9a3bd0039e48d4c36c28f1636007
Matt Hogg <matt.hogg74@hotmail.com>
2022-11-15 11:56:50 +0000

    HomePage is the actual home. Need to remove home.jsx

--- classic-cinema-company/src/App.js
+++ classic-cinema-company/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Home from './components/Home';
+import HomePage from './components/HomePage';
 import Schedule from './components/Schedule';
 import SignUp from './components/SignUp';
 import ComingSoon from "./components/ComingSoon";
@@ -16,7 +16,7 @@ function App() {
         <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={<HomePage />}/>
           <Route path='/schedule' element={<Schedule />}/>
           <Route path='/signup' element={<SignUp />}/>
           <Route path='/comingsoon' element={<ComingSoon />}></Route>

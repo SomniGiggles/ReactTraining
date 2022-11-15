7f01d8598dcab19cb453e1e8cc656a70539b0e63
andrewp2-digital <Andrew.P2@digital.ncsc.gov.uk>
2022-11-15 10:53:52 +0000

    updated routing

--- classic-cinema-company/src/components/header.jsx
+++ classic-cinema-company/src/components/header.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { Form } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import HomePage from "./HomePage";
-import WhatsOn from "./WhatsOn";
-import ComingSoon from "./ComingSoon";
-import SignUp from "./SignUp";
-import YourVisit from "./YourVisit";
-import BookTickets from "./BookTickets";
+
 import './Header.css';
 import './Common.css';
 
@@ -16,26 +10,16 @@ const Header = () => {
     //Some sort of image file at the top
 
     return <>
-        <Router>
             <nav style={{margin: 10}}>
-                <img src="/assets/CCC-logo.png" className="bannerLogo" />
-            <Link to='/' style={{padding: 5}}>Home</Link>
-            <Link to='/whatson' style={{padding: 5}}>What's On</Link>
-            <Link to='/comingsoon' style={{padding: 5}}>Coming Soon</Link>
-            <Link to='/signup' style={{padding: 5}}>Sign Up</Link>
-            <Link to='/yourvisit' style={{padding: 5}}>Your Visit</Link>
-            <Link to='/booktickets' style={{padding: 5}}>Book Tickets</Link>
+                <img src="/assets/CCC-logo.png" className="bannerLogo" alt='bannerLogo'/>
+            <NavLink to='/' style={{padding: 5}}>Home</NavLink>
+            <NavLink to='/schedule' style={{padding: 5}}>What's On</NavLink>
+            <NavLink to='/comingsoon' style={{padding: 5}}>Coming Soon</NavLink>
+            <NavLink to='/signup' style={{padding: 5}}>Sign Up</NavLink>
+            <NavLink to='/yourvisit' style={{padding: 5}}>Your Visit</NavLink>
+            <NavLink to='/booktickets' style={{padding: 5}}>Book Tickets</NavLink>
             <input type="text" placeholder="Search" />
             </nav>
-            <Routes>
-                <Route path='/' element={<HomePage />}></Route>
-                <Route path='/whatson' element={<WhatsOn />}></Route>
-                <Route path='/comingsoon' element={<ComingSoon />}></Route>
-                <Route path='/signup' element={<SignUp />}></Route>
-                <Route path='/yourvisit' element={<YourVisit />}></Route>
-                <Route path='/booktickets' element={<BookTickets />}></Route>
-            </Routes>
-        </Router>
 
     </>;
 };
